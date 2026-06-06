44ba20bbe2c3884263c8bdf21a555b7bc67ae596
eunjin.park <ejpark78@gmail.com>
2026-06-06 16:29:01 +0900

    feat: add Makefile for managing tool services with up and down commands

---
+++ scripts/tools.mk
@@ -0,0 +1,40 @@
+# ==============================================================================
+# 🗄️ Tools
+# ==============================================================================
+
+.PHONY: *
+
+tools-up: tools-up-kasm tools-up-mongo tools-up-redis tools-up-yacht tools-up-dozzle tools-up-cronicle tools-up-jupyter
+	@echo "✅ 모든 Tools가 실행되었습니다."
+
+tools-down:
+	$(COMPOSE) --profile tools down traefik kasm mongo-express redisinsight yacht dozzle cronicle jupyter
+	@echo "🛑 Tools가 중지되었습니다."
+
+tools-up-kasm:
+	$(COMPOSE) --profile tools up -d kasm
+	@echo "🚀 KASM VDI가 실행되었습니다. https://kasm.localhost 에 접속하세요."
+
+tools-up-mongo:
+	$(COMPOSE) --profile tools up -d mongo-express
+	@echo "🚀 Mongo Express GUI가 실행되었습니다. https://me.localhost 에 접속하세요."
+
+tools-up-redis:
+	$(COMPOSE) --profile tools up -d redisinsight
+	@echo "🚀 RedisInsight GUI가 실행되었습니다. https://redis.localhost 에 접속하세요."
+
+tools-up-yacht:
+	$(COMPOSE) --profile tools up -d yacht
+	@echo "🚀 Yacht GUI가 실행되었습니다. https://yacht.localhost 에 접속하세요."
+
+tools-up-dozzle:
+	$(COMPOSE) --profile tools up -d dozzle
+	@echo "🚀 Dozzle GUI가 실행되었습니다. https://dozzle.localhost 에 접속하세요."
+
+tools-up-cronicle:
+	$(COMPOSE) --profile tools up -d cronicle
+	@echo "🚀 Cronicle GUI가 실행되었습니다. https://cron.localhost 에 접속하세요."
+
+tools-up-jupyter:
+	$(COMPOSE) --profile tools up -d jupyter
+	@echo "🚀 Jupyter GUI가 실행되었습니다. https://jupyter.localhost 에 접속하세요."
