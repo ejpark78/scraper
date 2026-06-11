a0fcc5aff25abc221a5a8bf74d454a4f9ecb017b
eunjin.park <ejpark78@gmail.com>
2026-06-12 08:00:32 +0900

    fix: update service images and restart command in Docker configuration

--- scripts/utils/pipeline.mk
+++ scripts/utils/pipeline.mk
@@ -7,7 +7,7 @@
 SCALE ?= 3
 
 restart:
-	SCALE=$(SCALE) $(COMPOSE) up -d --build clipper-scraper clipper-transformer viewer
+	SCALE=$(SCALE) $(COMPOSE) up -d --build clipper clipper-scraper clipper-transformer
 
 clear-queue:
 	$(COMPOSE) run --rm $(RUN_USER) -v ./src/scripts:/app/src/scripts clipper npx ts-node src/scripts/clear-queue.ts
