7168a10176d48cd777f1089f9aad3543ac25c9da
eunjin.park <ejpark78@gmail.com>
2026-06-11 12:57:57 +0900

    feat: update scraper/transformer implementation

--- scripts/utils/pipeline.mk
+++ scripts/utils/pipeline.mk
@@ -11,3 +11,6 @@ restart:
 
 clear-queue:
 	$(COMPOSE) run --rm $(RUN_USER) -v ./src/scripts:/app/src/scripts clipper npx ts-node src/scripts/clear-queue.ts
+
+grep-error:
+	$(COMPOSE) logs --no-color clipper-scraper clipper-transformer | $(COMPOSE) run --rm -T $(RUN_USER) -v ./src/scripts:/app/src/scripts clipper npx ts-node src/scripts/grep-error.ts
