a4e92c39859d7386b33fb930b7262ad4b0c5a69b
eunjin.park <ejpark78@gmail.com>
2026-06-11 12:50:42 +0900

    feat: update scraper/transformer implementation

--- scripts/utils/pipeline.mk
+++ scripts/utils/pipeline.mk
@@ -8,3 +8,6 @@ SCALE ?= 3
 
 restart:
 	SCALE=$(SCALE) $(COMPOSE) up -d --build clipper-scraper clipper-transformer viewer
+
+clear-queue:
+	$(COMPOSE) run --rm $(RUN_USER) -v ./src/scripts:/app/src/scripts clipper npx ts-node src/scripts/clear-queue.ts
