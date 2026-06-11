6e88430eabf45eaaeeb7ff0cd2197d9e8d203df2
eunjin.park <ejpark78@gmail.com>
2026-06-11 11:33:24 +0900

    chore: format environment variables for consistency

--- scripts/environments.mk
+++ scripts/environments.mk
@@ -11,11 +11,11 @@ CHUNK_SIZE       ?= 500
 ERROR_RESET      ?= false
 
 # Runtime Execution Wrapper
-RUN_USER := --user $(shell id -u):$(shell id -g)
+RUN_USER         := --user $(shell id -u):$(shell id -g)
 export RUN_USER
 
 # Dynamic Environment Variable string construction
 # Note: We use a function-like approach to collect active variables
 # Since Makefile doesn't have easy array/map, we define the common set
-ENV_COMMON = -e LIST_SLACK=$(LIST_SLACK) -e SCRAPER_SLACK=$(SCRAPER_SLACK) -e PRIORITY=$(PRIORITY) -e OVERWRITE=$(OVERWRITE) -e RECURSIVE_SCRAPE=$(RECURSIVE_SCRAPE) -e AUTH=$(AUTH) -e CHUNK_SIZE=$(CHUNK_SIZE) -e ERROR_RESET=$(ERROR_RESET)
+ENV_COMMON        = -e LIST_SLACK=$(LIST_SLACK) -e SCRAPER_SLACK=$(SCRAPER_SLACK) -e PRIORITY=$(PRIORITY) -e OVERWRITE=$(OVERWRITE) -e RECURSIVE_SCRAPE=$(RECURSIVE_SCRAPE) -e AUTH=$(AUTH) -e CHUNK_SIZE=$(CHUNK_SIZE) -e ERROR_RESET=$(ERROR_RESET)
 export ENV_COMMON
