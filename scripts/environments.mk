f3b4e655ca7dcae0d5593508f6f68942f63cd27f
eunjin.park <ejpark78@gmail.com>
2026-06-15 13:30:37 +0900

    chore: commit changes

--- scripts/environments.mk
+++ scripts/environments.mk
@@ -9,6 +9,7 @@ RECURSIVE_SCRAPE ?= true
 AUTH             ?= false
 CHUNK_SIZE       ?= 500
 ERROR_RESET      ?= false
+RESET            ?= false
 
 # Runtime Execution Wrapper
 RUN_USER         := --user $(shell id -u):$(shell id -g)
@@ -22,5 +23,5 @@ export WORKSPACE_DIR WORKSPACE_MOUNT
 # Dynamic Environment Variable string construction
 # Note: We use a function-like approach to collect active variables
 # Since Makefile doesn't have easy array/map, we define the common set
-ENV_COMMON        = -e LIST_SLACK=$(LIST_SLACK) -e SCRAPER_SLACK=$(SCRAPER_SLACK) -e PRIORITY=$(PRIORITY) -e OVERWRITE=$(OVERWRITE) -e RECURSIVE_SCRAPE=$(RECURSIVE_SCRAPE) -e AUTH=$(AUTH) -e CHUNK_SIZE=$(CHUNK_SIZE) -e ERROR_RESET=$(ERROR_RESET)
+ENV_COMMON        = -e LIST_SLACK=$(LIST_SLACK) -e SCRAPER_SLACK=$(SCRAPER_SLACK) -e PRIORITY=$(PRIORITY) -e OVERWRITE=$(OVERWRITE) -e RECURSIVE_SCRAPE=$(RECURSIVE_SCRAPE) -e AUTH=$(AUTH) -e CHUNK_SIZE=$(CHUNK_SIZE) -e ERROR_RESET=$(ERROR_RESET) -e RESET=$(RESET)
 export ENV_COMMON
