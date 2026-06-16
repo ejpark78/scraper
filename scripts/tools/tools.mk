35935d5e3caa4213fd9803dd36b8a3bd9b400535
eunjin.park <ejpark78@gmail.com>
2026-06-16 16:13:09 +0900

    chore: commit changes

--- scripts/tools/tools.mk
+++ scripts/tools/tools.mk
@@ -46,8 +46,8 @@ up-jupyter:
 	@echo "🚀 Jupyter GUI가 실행되었습니다. https://jupyter.localhost 에 접속하세요."
 
 up-viewer:
-	$(COMPOSE) --profile tools build --no-cache viewer
-	$(COMPOSE) --profile tools up -d viewer
+	$(COMPOSE) --profile tools build --no-cache viewer-fe viewer-api viewer-mcp
+	$(COMPOSE) --profile tools up -d viewer-fe viewer-api viewer-mcp
 	@echo "🚀 Viewer GUI가 실행되었습니다. https://viewer.localhost 에 접속하세요."
 
 up-onwatch:
